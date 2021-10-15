3bc500e2d7f043c7e3644719f23f9d1909dfb33e
BalvirPurewal <balvirp1@gmail.com>
2021-10-14 20:52:40 -0500

    Added and using new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import { Adventures } from "./components/Adventures";
 import { Cover } from "./components/Cover";
+import { ExplorerSection } from "./components/ExplorerSection"
+import { ExplorerButton } from "./components/ExplorerButton";
+import { CallToAction } from "./components/CallToAction";
+import accomodationb from "../src/static/images/accomodationb.png";
+import experienceb from "../src/static/images/experienceb.png"
+import adventureb from "../src/static/images/adventureb.png"
+import { Container } from "./components/Container";
 
 function App() {
   return (
     <div className="App">
       <Cover></Cover>
+      <Container prefix="center">
+        <ExplorerSection title = "Explorer Chairdnb">
+          <ExplorerButton source={accomodationb}>Accommodations</ExplorerButton>
+          <ExplorerButton source={experienceb}>Experiences</ExplorerButton>
+          <ExplorerButton source={adventureb}>Adventures</ExplorerButton>
+        </ExplorerSection>
+        <CallToAction title = "Chairdnb Plus accommodation" subtle = "A selection of accommodation verified according to quality and connectivity criteria" />
+      </Container>
       <Adventures />
     </div>
   );
