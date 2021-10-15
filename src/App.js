dba4687989958a8b45301d4d96b98f8165e37891
BalvirPurewal <balvirp1@gmail.com>
2021-10-15 11:14:45 -0500

    Filter by place

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
     };
 
     fetchAll();
+    console.log("You fetch again")
   }, []);
 
   return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Form.css";
 
 export default function Form({data}) {
-  console.log(data);
+  const [place, setPlace] = useState("");
+
+
+  function search(e){
+    e.preventDefault();
+    console.log(data);
+    const hasPlace = place.length > 0 ? data.filter((site) => site.name.toUpperCase().includes(place.toUpperCase()) || site.country.toUpperCase().includes(place.toUpperCase())) : data;
+    console.log(hasPlace);
+  };
 
   return (
     <div className="form_box">
       <h2>Book unique accommodations to code as never before</h2>
-      <form className="form">
+      <form className="form" onSubmit={search} action="#">
         <label className="label-form block">Where</label>
-        <input className="input-form" type="text" placeholder="Everywhere" />
+        <input className="input-form" type="text" placeholder="Everywhere" onChange={(e) => setPlace(e.target.value)} value={place}/>
         <div className="flex">
           <div className="">
             <label className="label-form block">Arrival</label>
@@ -31,12 +40,13 @@ export default function Form({data}) {
         </div>
         <label className="label-form block">Developers</label>
         <input className="input-form" type="text" placeholder="1" />
+        <div className="btn-container">
+          <button className="btn-form" type="submit">
+            Search
+          </button>
+        </div>
       </form>
-      <div className="btn-container">
-        <button className="btn-form" type="submit">
-          Search
-        </button>
-      </div>
+      
     </div>
   );
 }
