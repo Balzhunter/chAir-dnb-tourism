d6ee015891372839447cb377b33d3b879e20ce18
Dulces123 <jregaladoantonio@gmail.com>
2021-10-15 15:29:49 -0500

    able to back home

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,20 +1,16 @@
-import React from "react";
-import ReactDOM from 'react-dom';
 import { LongCard } from "./LongCard";
 import { Container } from "./Container";
-import { useState } from "react";
 import { useEffect } from "react";
-import App from "../App"
 
 export function SearchResults({results, quantity, parentUpdate}) {
 
-  React.useEffect(() => {
+  useEffect(() => {
     document.querySelector('.cover').querySelector("h1").addEventListener('click', () => {
       parentUpdate(null)})
   }, [])
 
   return (
-    <div className="adventures" id = "Adventures">
+    <div className="adventures mt-38" id = "Adventures">
       <Container prefix="center">
         <Container prefix="long-card">
           {results.map((adventure) => <LongCard key={adventure.name} name={adventure.name} image_url={adventure.image_url} price={ adventure.price*(quantity == "" ? 1 : quantity)} rating={adventure.rating} />)}
