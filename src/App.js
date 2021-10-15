2475fab8832a036614db5499fa09adb602646600
Ana Isabel <anaisabel.mts@gmail.com>
2021-10-14 21:37:09 -0500

    Create medium cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import "./App.css";
 import LongCard from "./components/LongCard";
 import { Container } from "./components/Container";
+import { MediumCard } from "./components/MediumCard";
+
 
 function App() {
   return (
-    <div className="App">
-      <Container prefix="center">
-        <h2 className="heading">Discover Chairdnb adventures</h2>
-        <p className="subtitle">Multi-day hackatons organized by local experts with activities, meals and accommodation included</p>
-        <Container prefix="long-card">
-          <LongCard name="Bali" image_url="https://picsum.photos/id/1023/640" price="120.0" rating="4.6"/>
-          <LongCard name="Bali" image_url="https://picsum.photos/id/1038/640" price="120.0" rating="4.6"/>
-          <LongCard name="Bali" image_url="https://picsum.photos/id/1026/640" price="120.0" rating="4.6"/>
-          <LongCard name="Bali" image_url="https://picsum.photos/id/1021/640" price="120.0" rating="4.6"/>
-          <LongCard name="Bali" image_url="https://picsum.photos/id/1054/640" price="120.0" rating="4.6"/>
-          <LongCard name="Bali" image_url="https://picsum.photos/id/1058/640" price="120.0" rating="4.6"/>
+    <>
+      <div className="App">
+        <Container prefix="center">
+          <h2 className="heading">Discover Chairdnb adventures</h2>
+          <p className="subtitle">Multi-day hackatons organized by local experts with activities, meals and accommodation included</p>
+          <Container prefix="long-card">
+            <LongCard name="Bali" image_url="https://picsum.photos/id/1023/640" price="120.0" rating="4.6"/>
+            <LongCard name="Bali" image_url="https://picsum.photos/id/1038/640" price="120.0" rating="4.6"/>
+            <LongCard name="Bali" image_url="https://picsum.photos/id/1026/640" price="120.0" rating="4.6"/>
+            <LongCard name="Bali" image_url="https://picsum.photos/id/1021/640" price="120.0" rating="4.6"/>
+            <LongCard name="Bali" image_url="https://picsum.photos/id/1054/640" price="120.0" rating="4.6"/>
+            <LongCard name="Bali" image_url="https://picsum.photos/id/1058/640" price="120.0" rating="4.6"/>
+          </Container>
         </Container>
-      </Container>
-    </div>
+      </div>
+
+      <section className="App">
+        <Container prefix="center">
+          <h2 className="heading">Accommodation around the world</h2>
+          <Container prefix="medium-card">
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/1018/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/1047/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/100/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/1038/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/1018/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/1047/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/100/640" price="577.0" rating="4.96"/>
+            <MediumCard name="Tokyo" image_url="https://picsum.photos/id/1038/640" price="577.0" rating="4.96"/>
+          </Container>
+        </Container>
+      </section>  
+    </>
   );
 }
 
---
+++ src/components/MediumCard.js
@@ -0,0 +1,22 @@
+import pink_star from "../static/images/pink_star.svg"
+
+function MediumCard({name, image_url, price, rating }) {
+  return (
+    <div className="medium-card">
+      <img className="medium-card__image" src={image_url} />
+      <div className="medium-card__details">
+        <div className="medium-card__detail">
+          <p className="medium-card__host">superhost</p>
+          <p className="medium-card__name">{name}</p>
+        </div>
+        <div className="medium-card__stars">
+          <img className="medium-card__icon" src={pink_star}/>
+          <p>{rating}</p>
+        </div>
+      </div>
+      <p className="medium-card__price">From {Math.round(price * 2)} € / person - 3 days</p>
+    </div>
+  );
+}
+
+export { MediumCard };
