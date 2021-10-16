b27712e2a8571cd2b4c2cdcf75bf3524fbd7327e
BalvirPurewal <balvirp1@gmail.com>
2021-10-15 20:17:47 -0500

    Added all timers with cards changing in order and rate

--- src/components/Accommodations.js
+++ src/components/Accommodations.js
@@ -2,11 +2,12 @@ import React from "react";
 import { MediumCard } from "./MediumCard";
 import { Container } from "./Container";
 import {  getAllAccommodations } from "../services/accommodations_service"
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { filterArrays } from "../utils/array_filter";
 
 export function Accommodations() {
   const [accommodations, setAccommodations] = useState([]);
+  const [initial, setInitial] = useState(0);
 
   useEffect(() => {
     const fetchAccommodations = async () => {
@@ -16,12 +17,21 @@ export function Accommodations() {
     fetchAccommodations();
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const leng = 10;
+      const rate = 8;
+      setInitial(initial => (leng > initial + rate) ? (initial + rate) : (initial + rate - leng));
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [])
+
   return (
       <div id="Accommodations">
         <Container prefix="center">
           <h2 className="heading">Accommodation around the world</h2>
           <Container prefix="medium-card">
-            {accommodations.map((accommodation) => <MediumCard key={accommodation.name} name={accommodation.name} image_url={accommodation.image_url} price={accommodation.price*3} rating={accommodation.rating} />)}
+            {filterArrays(accommodations, 8, initial).map((accommodation) => <MediumCard key={accommodation.name} name={accommodation.name} image_url={accommodation.image_url} price={accommodation.price*3} rating={accommodation.rating} />)}
           </Container>
         </Container>
       </div>
