62633048f96d0b7269c2a06dd3ff306dfa5c50db
BalvirPurewal <balvirp1@gmail.com>
2021-10-15 19:23:55 -0500

    Geting 6 then 4

--- src/components/Adventures.js
+++ src/components/Adventures.js
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 
 export function Adventures() {
   const [adventures, setAdventures] = useState([]);
+  const [timer, setTimer] = useState(0);
 
   useEffect(() => {
     const fetchAdventures = async () => {
@@ -16,13 +17,21 @@ export function Adventures() {
     fetchAdventures();
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer => timer + 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [])
+
+  console.log(timer)
   return (
     <div className="adventures" id = "Adventures">
       <Container prefix="center">
         <h2 className="heading">Discover Chairdnb adventures</h2>
         <p className="subtitle">Multi-day hackatons organized by local experts with activities, meals and accommodation included</p>
         <Container prefix="long-card">
-          {adventures.map((adventure) => <LongCard key={adventure.name} name={adventure.name} image_url={adventure.image_url} price={adventure.price*3} rating={adventure.rating} />)}
+          {(timer % 2 === 0 ? adventures.slice(0,6) : adventures.slice(6,10)).map((adventure) => <LongCard key={adventure.name} name={adventure.name} image_url={adventure.image_url} price={adventure.price*3} rating={adventure.rating} />)}
         </Container>
       </Container>
     </div>
